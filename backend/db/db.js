1070d3524ca11387159809db5236b1e7b488d80a
Manohar Yekkala <manohar.yekkala@gmail.com>
2025-05-24 14:17:13 +0530

    mongo uri

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   return mongoose
-    .connect(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_SECRET}@contact-manage.36vmmow.mongodb.net/?retryWrites=true&w=majority`
-    )
+    .connect(`${process.env.MONGODB_URI}`)
     .then(() => {
       console.log("connection established");
     })
