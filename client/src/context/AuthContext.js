e4368a9b23360a972cd5a95c551232a970a4b298
Manohar Yekkala <manohar.yekkala@gmail.com>
2025-05-24 14:31:02 +0530

    api changes

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -21,7 +21,7 @@ export const AuthContextProvider = ({ children }) => {
 
   const isUserLoggedIn = async () => {
     try {
-      const res = await fetch(`https://contact-api.up.railway.app/api/user`, {
+      const res = await fetch(`${process.env.HOST}/api/user`, {
         method: "GET",
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -49,7 +49,7 @@ export const AuthContextProvider = ({ children }) => {
   //login
   const loginUser = async (userData) => {
     try {
-      const res = await fetch(`https://contact-api.up.railway.app/api/login`, {
+      const res = await fetch(`${process.env.HOST}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...userData }),
@@ -79,14 +79,11 @@ export const AuthContextProvider = ({ children }) => {
   //register
   const registerUser = async (userData) => {
     try {
-      const res = await fetch(
-        `https://contact-api.up.railway.app/api/register`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ ...userData }),
-        }
-      );
+      const res = await fetch(`${process.env.HOST}/api/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...userData }),
+      });
       const result = await res.json();
       if (!result.error) {
         toast.success("Successfully registered, Login into you account");
